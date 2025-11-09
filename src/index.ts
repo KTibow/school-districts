7985d868043bddf441f62606b3266b889e4cd3f7
KTibow <KTibow@users.noreply.github.com>
2025-11-09 10:28:08 -0800

    v3: tsdown, school data, tree shaking

---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import { locations as nsdLocations, nws as nsdNWS } from "./schools/nsd";
+
+const weekdayRange = (from: string, to: string) => {
+  const startDate = new Date(from);
+  const endDate = new Date(to);
+  const dates = [];
+
+  const currentDate = new Date(startDate.getTime());
+
+  while (currentDate <= endDate) {
+    const dayOfWeek = currentDate.getDay();
+    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+      dates.push(new Date(currentDate.getTime()));
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+};
+const subtractRanges = (base: Date[], ...subtractions: Date[]) =>
+  base.filter((date) => {
+    return !subtractions.some((subtraction) => {
+      return date.getTime() === subtraction.getTime();
+    });
+  });
+const selectSemester = (semesters: Date[][]) =>
+  semesters.find((s) => s[0] < new Date() && new Date() < s[s.length - 1]) ||
+  semesters[0];
+
+export const districtNames: Record<string, string> = {
+  "apps.nsd.org": "NSD",
+};
+
+export const districtExpandedNames: Record<string, string> = {
+  "apps.nsd.org": "Northshore School District",
+};
+
+export const districtLocations: Record<string, string> = {
+  "apps.nsd.org": "Washington State",
+};
+
+export const districtApps: Record<string, { app: string; base: string }[]> = {
+  "apps.nsd.org": [
+    {
+      app: "StudentVue",
+      base: "https://wa-nor-psv.edupoint.com",
+    },
+  ],
+};
+
+export const districtSemesters: Record<string, Date[][]> = {
+  "apps.nsd.org": [
+    // First semester: Sept 3 - Jan 23
+    subtractRanges(
+      weekdayRange("2025-09-03", "2026-01-23"),
+      // Veterans Day
+      new Date("2025-11-11"),
+      // Thanksgiving week
+      ...weekdayRange("2025-11-27", "2025-11-28"),
+      // Winter break
+      ...weekdayRange("2025-12-22", "2026-01-02"),
+      // MLK Day
+      new Date("2026-01-19"),
+    ),
+    // Second semester: Jan 26 - June 17
+    subtractRanges(
+      weekdayRange("2026-01-26", "2026-06-17"),
+      new Date("2026-01-26"),
+      // President's Day
+      new Date("2026-02-16"),
+      // Mid-winter break
+      ...weekdayRange("2026-02-17", "2026-02-20"),
+      // Spring break
+      ...weekdayRange("2026-04-06", "2026-04-10"),
+      // Memorial Day
+      new Date("2026-05-25"),
+    ),
+  ],
+};
+
+export const districtCurrentSemesters: Record<string, Date[]> = {
+  "apps.nsd.org": selectSemester(districtSemesters["apps.nsd.org"]),
+};
+
+export const schoolLocations: Record<
+  string,
+  Record<string, [number, number]>
+> = {
+  "apps.nsd.org": nsdLocations,
+};
+
+export const schoolNWS: Record<string, Record<string, string>> = {
+  "apps.nsd.org": nsdNWS,
+};
+
+// You can add your district :)
---
+++ src/schools/nsd.ts
@@ -0,0 +1,100 @@
+export const locations: Record<string, [number, number]> = {
+  "Bothell High School": [47.760475, -122.2205602],
+  "Inglemoor High School": [47.7414843, -122.222623],
+  "Innovation Lab High School": [47.7942343, -122.2031389],
+  "North Creek High School": [47.824758, -122.1812311],
+  "Woodinville High School": [47.7711163, -122.1613185],
+  // ---
+  "Canyon Park Middle School": [47.7828811, -122.1995786],
+  "Kenmore Middle School": [47.7765432, -122.255137],
+  "Leota Middle School": [47.7669461, -122.1174232],
+  "Northshore Middle School": [47.7426653, -122.179254],
+  "Skyview Middle School": [47.8030504, -122.1889745],
+  "Timbercrest Middle School": [47.7662718, -122.0517364],
+  // ---
+  "Arrowhead Elementary": [47.7369496, -122.251694],
+  "Canyon Creek Elementary": [47.8050348, -122.1889107],
+  "Cottage Lake Elementary": [47.745061, -122.0780426],
+  "Crystal Springs Elementary": [47.8010984, -122.2196366],
+  "East Ridge Elementary": [47.7437696, -122.0430072],
+  "Fernwood Elementary": [47.8200944, -122.1791088],
+  "Frank Love Elementary": [47.7959145, -122.2370086],
+  "Hollywood Hill Elementary": [47.7517907, -122.1392922],
+  "Kenmore Elementary": [47.7674434, -122.2426281],
+  "Kokanee Elementary": [47.7823842, -122.1592001],
+  "Lockwood Elementary": [47.7784495, -122.2559603],
+  "Maywood Hills Elementary": [47.769988, -122.1978778],
+  "Moorlands Elementary": [47.7385329, -122.2308941],
+  "Ruby Bridges Elementary": [47.8111036, -122.1676301],
+  "Shelton View Elementary": [47.7861827, -122.2396946],
+  "Sunrise Elementary": [47.7280193, -122.1086083],
+  "Wellington Elementary": [47.7674549, -122.1202292],
+  "Westhill Elementary": [47.7702635, -122.2255031],
+  "Woodin Elementary": [47.7678866, -122.1672296],
+  "Woodmoor Elementary": [47.7418199, -122.1765075],
+};
+export const nws: Record<string, string> = {
+  "Bothell High School":
+    "https://api.weather.gov/gridpoints/SEW/130,75/forecast",
+  "Inglemoor High School":
+    "https://api.weather.gov/gridpoints/SEW/129,74/forecast",
+  "Innovation Lab High School":
+    "https://api.weather.gov/gridpoints/SEW/130,76/forecast",
+  "North Creek High School":
+    "https://api.weather.gov/gridpoints/SEW/131,77/forecast",
+  "Woodinville High School":
+    "https://api.weather.gov/gridpoints/SEW/132,75/forecast",
+  // ---
+  "Canyon Park Middle School":
+    "https://api.weather.gov/gridpoints/SEW/130,75/forecast",
+  "Kenmore Middle School":
+    "https://api.weather.gov/gridpoints/SEW/129,75/forecast",
+  "Leota Middle School":
+    "https://api.weather.gov/gridpoints/SEW/133,74/forecast",
+  "Northshore Middle School":
+    "https://api.weather.gov/gridpoints/SEW/131,73/forecast",
+  "Skyview Middle School":
+    "https://api.weather.gov/gridpoints/SEW/131,76/forecast",
+  "Timbercrest Middle School":
+    "https://api.weather.gov/gridpoints/SEW/135,74/forecast",
+  // ---
+  "Arrowhead Elementary":
+    "https://api.weather.gov/gridpoints/SEW/128,74/forecast",
+  "Canyon Creek Elementary":
+    "https://api.weather.gov/gridpoints/SEW/131,76/forecast",
+  "Cottage Lake Elementary":
+    "https://api.weather.gov/gridpoints/SEW/134,73/forecast",
+  "Crystal Springs Elementary":
+    "https://api.weather.gov/gridpoints/SEW/130,76/forecast",
+  "East Ridge Elementary":
+    "https://api.weather.gov/gridpoints/SEW/135,73/forecast",
+  "Fernwood Elementary":
+    "https://api.weather.gov/gridpoints/SEW/131,77/forecast",
+  "Frank Love Elementary":
+    "https://api.weather.gov/gridpoints/SEW/129,76/forecast",
+  "Hollywood Hill Elementary":
+    "https://api.weather.gov/gridpoints/SEW/132,74/forecast",
+  "Kenmore Elementary":
+    "https://api.weather.gov/gridpoints/SEW/129,75/forecast",
+  "Kokanee Elementary":
+    "https://api.weather.gov/gridpoints/SEW/132,75/forecast",
+  "Lockwood Elementary":
+    "https://api.weather.gov/gridpoints/SEW/129,76/forecast",
+  "Maywood Hills Elementary":
+    "https://api.weather.gov/gridpoints/SEW/130,75/forecast",
+  "Moorlands Elementary":
+    "https://api.weather.gov/gridpoints/SEW/129,74/forecast",
+  "Ruby Bridges Elementary":
+    "https://api.weather.gov/gridpoints/SEW/132,77/forecast",
+  "Shelton View Elementary":
+    "https://api.weather.gov/gridpoints/SEW/129,76/forecast",
+  "Sunrise Elementary":
+    "https://api.weather.gov/gridpoints/SEW/133,72/forecast",
+  "Wellington Elementary":
+    "https://api.weather.gov/gridpoints/SEW/133,74/forecast",
+  "Westhill Elementary":
+    "https://api.weather.gov/gridpoints/SEW/130,75/forecast",
+  "Woodin Elementary": "https://api.weather.gov/gridpoints/SEW/131,75/forecast",
+  "Woodmoor Elementary":
+    "https://api.weather.gov/gridpoints/SEW/131,73/forecast",
+};
